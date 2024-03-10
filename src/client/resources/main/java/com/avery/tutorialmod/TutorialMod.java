9a070b56bbd035092debf30a8f3eb18cb9ee17bb
TheGoodChild <thegoodchild05@gmail.com>
2024-03-10 12:25:25 -0400

    messed up and reimported some stuff, all it did was change the names of some things, still works tho

--- src/client/resources/main/java/com/avery/tutorialmod/TutorialMod.java
+++ src/client/resources/main/java/com/avery/tutorialmod/TutorialMod.java
@@ -17,12 +17,12 @@ public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	//public static final Block HEHE  = new Block(FabricBlockSettings.create().strength(4.0f));
+	public static final Block HEHE  = new Block(FabricBlockSettings.create().strength(4.0f));
 
 	@Override
 	public void onInitialize() {
-		//Registry.register(Registries.BLOCK, new Identifier("tutorial", "BestBlock"), HEHE);
-		//Registry.register(Registries.ITEM, new Identifier("tutorial", "example_block"), new BlockItem(HEHE, new FabricItemSettings()));
+		Registry.register(Registries.BLOCK, new Identifier("tutorial", "bestblock"), HEHE);
+		Registry.register(Registries.ITEM, new Identifier("tutorial", "example_block"), new BlockItem(HEHE, new FabricItemSettings()));
 		LOGGER.info("Hello Fabric world!");
 	}
 }
